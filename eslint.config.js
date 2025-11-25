d131a03f32f0c62fa1e4628e5c115f77a7aac12d
Iryna.Valadzko <ice7aye@gmail.com>
2025-11-25 16:59:23 +0300

    chore(eslint): remove eslint-plugin-prettier integration

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,15 @@
 import { defineConfig } from 'eslint/config';
-import prettier from 'eslint-plugin-prettier';
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import globals from 'globals';
-import reactRefresh from 'typescript-eslint';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default defineConfig([
   js.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    plugins: {
-      prettier,
-    },
-    rules: {
-      'prettier/prettier': 'error',
-    },
     ignores: ['node_modules/', 'dist/', 'build/', 'coverage/'],
   },
-
   {
     files: ['frontend/**/*.{ts,tsx,js,jsx}'],
     ...reactRefresh.configs.vite,
