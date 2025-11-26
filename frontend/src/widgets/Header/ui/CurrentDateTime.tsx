19e19d297d3f9c1ff2b71ac39a39149811306588
Iryna.Valadzko <ice7aye@gmail.com>
2025-11-26 17:57:17 +0300

    feat(header): add CurrentDateTime component
    - Implemented component to display current date and time

--- frontend/src/widgets/Header/ui/CurrentDateTime.tsx
+++ frontend/src/widgets/Header/ui/CurrentDateTime.tsx
@@ -1,3 +1,38 @@
+import { useEffect, useState } from 'react';
+
 export const CurrentDateTime = () => {
-  return <h2>date and time Component</h2>;
+  const [dateTime, setDateTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setDateTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  const userLocale: string = navigator.language || 'en-GB';
+
+  const formattedDate: string = dateTime.toLocaleDateString(userLocale, {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
+
+  const formattedWeekday: string = dateTime.toLocaleDateString(userLocale, {
+    weekday: 'long',
+  });
+
+  const formattedTime: string = dateTime.toLocaleTimeString(userLocale, {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+  });
+  return (
+    <div>
+      <div>{formattedDate}</div>
+      <div>{formattedWeekday}</div>
+      <div>{formattedTime}</div>
+    </div>
+  );
 };
